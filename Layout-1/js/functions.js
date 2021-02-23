9e59152072bf84a781dd65c2ba8f470346b7f30d
Diogo-Valerio <34871369+Diogo-Valerio@users.noreply.github.com>
2021-02-23 12:23:48 -0300

    Update Layout

---
+++ Layout-1/js/functions.js
@@ -0,0 +1,14 @@
+function openSlideMenu(){
+
+  document.getElementById('menu').style.width = '250px';
+  document.getElementById('contente').style.marginLeft = '250px';
+  
+  
+  }
+
+  function closeSlideMenu(){
+  document.getElementById('menu').style.width = '0';
+  document.getElementById('contente').style.marginLeft = '0';
+  }
+
+ 
